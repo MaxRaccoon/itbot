ce335557e9b17513b9163b06fa76fd82e2512381
MaxRaccoon <enot.work@gmail.com>
2025-09-01 15:43:51 +0800

    PiggyBank

--- src/Command/SetWebhookCommand.php
+++ src/Command/SetWebhookCommand.php
@@ -5,23 +5,25 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpClient\HttpClient;
 
 class SetWebhookCommand extends Command
 {
     protected static $defaultName = 'app:set-webhook';
+    private $params;
 
-    public function __construct()
+    public function __construct(ParameterBagInterface $params)
     {
+        $this->params = $params;
         parent::__construct(self::$defaultName);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $token = getenv('TELEGRAM_BOT_TOKEN');
-        $domain = getenv('APP_DOMAIN');
+        $token = $this->params->get('telegram_token');
+        $domain = $this->params->get('main_domain');
         $webhookUrl = 'https://' . $domain . '/telegram/webhook';
-
         $client = HttpClient::create();
         $response = $client->request('GET', "https://api.telegram.org/bot{$token}/setWebhook", [
             'query' => [
