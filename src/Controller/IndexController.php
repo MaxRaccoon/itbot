417915ff84f4e62ec86471b66d8aa07db0d59106
MaxRaccoon <enot.work@gmail.com>
2025-09-01 15:11:28 +0800

    PiggyBank

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,10 +1,7 @@
 <?php
 namespace App\Controller;
 
-use App\Entity\Tests;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
